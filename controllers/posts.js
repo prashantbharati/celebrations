da1a61d50475422bdc648bea20a653999846ec3a
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-01-23 17:03:37 +0530

    adding controllers tags

--- controllers/posts.js
+++ controllers/posts.js
@@ -48,6 +48,22 @@ export const updatePost = async (req, res) => {
   res.json(updatedPost);
 };
 
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+
+  try {
+    const title = new RegExp(searchQuery, "i");
+
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(",") } }],
+    });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const deletePost = async (req, res) => {
   const { id } = req.params;
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getPosts,
+  getPostsBySearch,
   createPost,
   updatePost,
   deletePost,
