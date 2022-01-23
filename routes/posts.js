a5c6fdb7d25c06791224c6c11ce8226bfd3fae4c
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-01-23 17:00:08 +0530

    adding route

--- routes/posts.js
+++ routes/posts.js
@@ -9,6 +9,7 @@ import {
 import auth from "../middleware/auth.js";
 const router = express.Router();
 
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
