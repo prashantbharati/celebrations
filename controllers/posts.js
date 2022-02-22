f6ee5e5ba6a69588aeb57110a75d4c3e0eebb7f6
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-02-22 11:12:57 +0530

    max limit set up

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,6 +7,9 @@ const router = express.Router();
 export const getPosts = async (req, res) => {
   const { page } = req.query;
   try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+
     const postMessages = await PostMessage.find();
 
     res.status(200).json(postMessages);
